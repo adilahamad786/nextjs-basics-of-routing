6223d27a18f0e1be93e7906b4123e0e468339b3f
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-27 11:52:46 +0530

    Bad way to navigate between pages using anchor tags: problems :- reload, fetch every page from server.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,17 @@
 // your domain.com/news
 
 const NewsPage = () => {
-    return <p>The news page</p>
+    return <>
+        <p>The news page</p>
+        <ul>
+            <li>
+                <a href="/news/nextjs-artical">Next.js-artical</a>
+            </li>
+            <li>
+                <a href="/news/reactjs-artical">React.js-artical</a>
+            </li>
+        </ul>
+    </>
 };
   
 export default NewsPage;
